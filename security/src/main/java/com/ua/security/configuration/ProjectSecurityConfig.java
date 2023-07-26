f282734fb690744b293069c48e253632e6b90627
DenysLeonenko <leonenkodendev@gmail.com>
2023-07-27 00:10:44 +0300

    [SECBANK-1] added primitive security configuration

--- security/src/main/java/com/ua/security/configuration/ProjectSecurityConfig.java
+++ security/src/main/java/com/ua/security/configuration/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.ua.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,11 +11,11 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
-        http.formLogin();
-        http.httpBasic();
+        http.authorizeHttpRequests((request) -> request
+                        .requestMatchers("/myAccount/**", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contacts").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
